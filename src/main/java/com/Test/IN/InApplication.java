748e649ca2596649be1ee5d27ab207c465a95bac
lenovo <lenovo@DESKTOP-6QEKUH4>
2023-03-10 16:24:16 +0530

    com

---
+++ src/main/java/com/Test/IN/InApplication.java
@@ -0,0 +1,13 @@
+package com.Test.IN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Test/IN/InApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Test.IN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
